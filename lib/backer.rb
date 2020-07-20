715c011abefefabed63fa4c7eb66d6d92eda1839
William Han Findiesen <william.findiesen@gmail.com>
2020-07-20 03:40:57 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,9 +1,9 @@
 class Backer
   
-  attr_accessor :name
+  attr_reader :name
   attr_accessor :backed_projects
   
-  def initialoze(name)
+  def initialize(name)
     @name = name
     @backed_projects = []
   end
